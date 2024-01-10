b9c8472fe506d785d9f3f08cd86f009292c7457e
liuliu <liuliugit@gmail.com>
2024-01-10 19:51:46 +0800

    feat: add essential APIs of Medium

---
+++ apis.py
@@ -0,0 +1,16 @@
+import requests
+from typing import Dict, List
+
+def getUserByToken(headers: Dict, timeout: int = 10) -> Dict:
+    response = requests.get('https://api.medium.com/v1/me', headers=headers, timeout=timeout)
+
+    response.raise_for_status()
+
+    return response.json()
+
+def createPost(author_id: str, data: List, headers: Dict, timeout: int = 10) -> Dict:
+    response = requests.post(f'https://api.medium.com/v1/users/{author_id}/posts', data=data, headers=headers, timeout=timeout)
+
+    response.raise_for_status()
+
+    return response.json()
