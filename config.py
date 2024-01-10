80b88a8e42a752a5a7523a181cd94fa9bc0c916b
liuliu <liuliugit@gmail.com>
2024-01-10 20:11:06 +0800

    feat: add config file for API constants

---
+++ config.py
@@ -0,0 +1,8 @@
+CONTENT_TYPE_HTML = 'html'
+CONTENT_TYPE_MARKDOWN = 'markdown'
+
+PUBLISH_STATUS_PUBLIC = 'public'
+PUBLISH_STATUS_DRAFT = 'draft'
+PUBLISH_STATUS_UNLISTED = 'unlisted'
+
+LICENSE_ALL = 'all-rights-reserved'
