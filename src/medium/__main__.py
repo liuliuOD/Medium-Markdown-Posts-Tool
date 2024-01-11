23899643a64cf532811a879960c7560dc7e57f3a
liuliu <liuliugit@gmail.com>
2024-01-11 21:02:36 +0800

    feat: add option tags to allow tags setting for post

--- src/medium/__main__.py
+++ src/medium/__main__.py
@@ -1,11 +1,13 @@
 import click
 from . import apis, config
+from typing import List
 
 @click.command()
-@click.option('-t', '--title', default='Draft', help='Title of the post. The default is `Draft`.')
+@click.option('--title', default='Draft', help='Title of the post. The default is `Draft`.')
 @click.option('-f', '--file', default='README.md', help='The filepath used to create the post. The default is `README.md`.')
 @click.option('-H', '--html', is_flag=True, help='The format of file content. The default is `markdown` if this flag is not set.')
-def commands(title: str, file: str, html: bool):
+@click.option('-t', '--tags', multiple=True, default=[], help='The tags you want to set to the post.')
+def commands(title: str, file: str, html: bool, tags: List):
     """
     Create Medium posts effortlessly using the CLI.
     """
@@ -15,7 +17,7 @@ def commands(title: str, file: str, html: bool):
             'title': title,
             'contentFormat': config.CONTENT_TYPE_HTML if html else config.CONTENT_TYPE_MARKDOWN,
             'content': file.read(),
-            'tags': [],
+            'tags': tags,
             'publishStatus': config.PUBLISH_STATUS_DRAFT,
             'license': config.LICENSE_ALL,
             'notifyFollowers': True
