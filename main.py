8fe79aa135c0a6a0f7465d411ca49b2174dc9fc5
liuliu <liuliugit@gmail.com>
2024-01-13 02:01:41 +0800

    refactor: assemble the data used to create medium post

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from src.medium import apis, config, formatter
 if __name__ == '__main__':
     author_id = apis.getUserByToken(config.HEADERS)['data']['id']
     with open('README.md', 'r') as file:
-        data = formatter.assembleDataForMediumPost(title='draft', html=False, file=file, tags=(), public=False, notify=False)
+        data = formatter.assembleCommandsData(title='draft', html=False, file=file, tags=(), public=False, notify=False)
         post = apis.createPost(author_id, data, config.HEADERS)
         print('New Post Link: {}'.format(post['data']['url']))
--- src/medium/formatter.py
+++ src/medium/formatter.py
@@ -1,15 +1,21 @@
+import typing
 from . import config
-from typing import Dict
 from io import TextIOBase
 from click import STRING, BOOL, Tuple
 
-def assembleDataForMediumPost(title: STRING, html: BOOL, file: TextIOBase, tags: Tuple, public: BOOL, notify: BOOL) -> Dict:
+def assembleCommandsData(title: STRING, html: BOOL, file: TextIOBase, tags: Tuple, public: BOOL, notify: BOOL) -> typing.Dict:
+    content_format = config.CONTENT_TYPE_HTML if html else config.CONTENT_TYPE_MARKDOWN
+    publish_status = config.PUBLISH_STATUS_PUBLIC if public else config.PUBLISH_STATUS_DRAFT
+
+    return assembleDataForMediumPost(title, content_format, file, tags, publish_status, notify)
+
+def assembleDataForMediumPost(title: str, content_format: str, file: TextIOBase, tags: typing.Tuple, publish_status: str, notify: bool) -> typing.Dict:
     return {
         'title': title,
-        'contentFormat': config.CONTENT_TYPE_HTML if html else config.CONTENT_TYPE_MARKDOWN,
+        'contentFormat': content_format,
         'content': file.read(),
         'tags': tags,
-        'publishStatus': config.PUBLISH_STATUS_PUBLIC if public else config.PUBLISH_STATUS_DRAFT,
+        'publishStatus': publish_status,
         'license': config.LICENSE_ALL,
         'notifyFollowers': notify
     }
