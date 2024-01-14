2ae5419cd407d46cbe55f41a7e46b7fccaa01286
liuliu <liuliugit@gmail.com>
2024-01-14 13:51:03 +0800

    refactor: demo codes in main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from src.medium import apis, config, formatter, services
+from src.medium import formatter, services
 
 if __name__ == '__main__':
-    # author_id = apis.getUserByToken(config.HEADERS)['data']['id']
-    # with open('README.md', 'r') as file:
-    #     data = formatter.assembleCommandsData(title='draft', html=False, file=file.read(), tags=(), public=False, notify=False)
-    #     post = apis.createPost(author_id, data, config.HEADERS)
-    #     print('New Post Link: {}'.format(post['data']['url']))
-
     with open('README.md', 'r') as file:
-        data = file.read()
-        data = services.uploadImagesInMarkdown(data)
-
+        file_content = file.read()
+        file_content = services.uploadImagesInMarkdown(file_content)
+        data = formatter.assembleCommandsData(title='draft', html=False, file_content=file_content, tags=(), public=False, notify=False)
         print(data)
+
+        post = services.createPost(data)
+        print('New Post Link: {}'.format(formatter.getUrlFromResponse(post)))
